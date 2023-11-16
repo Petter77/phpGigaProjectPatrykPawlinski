b9d5b1e508dfb4fc1084d7efcd5f160091e0ae48
Petter77 <ppawlinski77@gmail.com>
2023-11-16 20:25:49 +0100

    Created basic navigation bar

--- index.php
+++ index.php
@@ -1,6 +1,11 @@
 <!doctype html>
 <html lang="pl">
 <head>
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap"
+          rel="stylesheet">
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
@@ -8,7 +13,18 @@
     <title>JTPPPCWOW</title>
 </head>
 <body>
-    <nav></nav>
-    <main></main>
+<nav>
+    <div id="nav-left">
+        <div id="logo">logo</div>
+    </div>
+    <div id="nav-right">
+            <div class="nav-elem">placeholder</div>
+            <div class="nav-elem">placeholder</div>
+            <div class="nav-elem">placeholder</div>
+    </div>
+</nav>
+<main>
+    
+</main>
 </body>
 </html>
