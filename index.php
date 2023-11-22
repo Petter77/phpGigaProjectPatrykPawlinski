342beb001ad9185e49a67931a21f9693e8486ad6
Petter77 <ppawlinski77@gmail.com>
2023-11-22 23:51:44 +0100

    Moved nav and footer to main div, code refactoring

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html lang="pl">
 <head>
-    <link rel="stylesheet" href="indexStyle.css">
+    <link rel="stylesheet" href="style.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap"
@@ -14,6 +14,7 @@
     <title>JTPPPCWOW</title>
 </head>
     <body>
+    <main>
         <nav>
             <div id="nav-left">
                 <div id="logo">
@@ -26,7 +27,6 @@
                 <a href="" id="login-button" class="nav-elem"><div>Login</div></a>
             </div>
         </nav>
-    <main>
         <div id="middle-section">
             <div class="post">
                 <div class="post-author">
@@ -36,25 +36,11 @@
                     Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad autem consequatur dignissimos facilis ipsam officia porro possimus, praesentium ratione vero. Animi atque autem cumque deleniti dicta distinctio nemo nihil nisi, nostrum nulla, odit omnis perferendis recusandae sapiente soluta temporibus veritatis!
                 </div>
             </div>
-            <div class="post">
-                <div class="post-author">
-                    Test
-                </div>
-                <div class="post-content">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. A, accusantium animi, commodi consequatur ducimus eos ex excepturi exercitationem id laboriosam maiores non nulla optio quam quidem rem repellendus rerum tempore voluptas voluptate? A ad aliquid autem, culpa dicta dolores earum fugit illum in maxime molestiae nesciunt nihil nobis nostrum nulla optio, pariatur quas quia quo repellendus similique soluta sunt suscipit tenetur veritatis. Aspernatur beatae cumque, dolores incidunt maxime nostrum quae quibusdam quos reiciendis, tenetur unde velit. Dolor et ipsa maiores, placeat quos sunt veniam voluptatem? Dolore doloremque ea molestias quam velit! At doloremque magni optio praesentium provident sapiente soluta voluptatum.
-                </div>
-            </div>
-            <div class="post">
-                <div class="post-author">
-                    Test
-                </div>
-                <div class="post-content">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. A, accusantium animi, commodi consequatur ducimus eos ex excepturi exercitationem id laboriosam maiores non nulla optio quam quidem rem repellendus rerum tempore voluptas voluptate? A ad aliquid autem, culpa dicta dolores earum fugit illum in maxime molestiae nesciunt nihil nobis nostrum nulla optio, pariatur quas quia quo repellendus similique soluta sunt suscipit tenetur veritatis. Aspernatur beatae cumque, dolores incidunt maxime nostrum quae quibusdam quos reiciendis, tenetur unde velit. Dolor et ipsa maiores, placeat quos sunt veniam voluptatem? Dolore doloremque ea molestias quam velit! At doloremque magni optio praesentium provident sapiente soluta voluptatum.
-                </div>
-            </div>
+        </div>
+            <footer>
+                <div>Icons made from <a href="https://www.onlinewebfonts.com/icon">svg icons</a> is licensed by CC BY 4.0</div>
+            </footer>
     </main>
-        <footer>
-            <div>Icons made from <a href="https://www.onlinewebfonts.com/icon">svg icons</a> is licensed by CC BY 4.0</div>
-        </footer>
+        
     </body>
 </html>
