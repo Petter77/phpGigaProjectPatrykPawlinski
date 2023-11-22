a3e900fa03174149b657b90abe398eaf557ac0e3
Petter77 <ppawlinski77@gmail.com>
2023-11-22 19:41:47 +0100

    Created basic template of post and changed page colors

--- index.php
+++ index.php
@@ -7,24 +7,42 @@
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap"
           rel="stylesheet">
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="author" content="Patryk Pawliński">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>JTPPPCWOW</title>
 </head>
-<body>
-<nav>
-    <div id="nav-left">
-        <div id="logo">logo</div>
-    </div>
-    <div id="nav-right">
-            <div class="nav-elem">placeholder</div>
-            <div class="nav-elem">placeholder</div>
-            <div class="nav-elem">placeholder</div>
-    </div>
-</nav>
-<main>
-    
-</main>
-</body>
+    <body>
+        <nav>
+            <div id="nav-left">
+                <div id="logo">logo</div>
+            </div>
+            <div id="nav-right">
+                <a href="" class="nav-elem"><div></div></a>
+                <a href="" id="register-button" class="nav-elem"><div>Register</div></a>
+                <a href="" id="login-button" class="nav-elem"><div>Login</div></a>
+            </div>
+        </nav>
+    <main>
+        <div id="middle-section">
+            <div class="post">
+                <div class="post-author">
+                    Petter
+                </div>
+                <div class="post-content">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad autem consequatur dignissimos facilis ipsam officia porro possimus, praesentium ratione vero. Animi atque autem cumque deleniti dicta distinctio nemo nihil nisi, nostrum nulla, odit omnis perferendis recusandae sapiente soluta temporibus veritatis!
+                </div>
+            </div>
+            <div class="post">
+                <div class="post-author">
+                    Test
+                </div>
+                <div class="post-content">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. A, accusantium animi, commodi consequatur ducimus eos ex excepturi exercitationem id laboriosam maiores non nulla optio quam quidem rem repellendus rerum tempore voluptas voluptate? A ad aliquid autem, culpa dicta dolores earum fugit illum in maxime molestiae nesciunt nihil nobis nostrum nulla optio, pariatur quas quia quo repellendus similique soluta sunt suscipit tenetur veritatis. Aspernatur beatae cumque, dolores incidunt maxime nostrum quae quibusdam quos reiciendis, tenetur unde velit. Dolor et ipsa maiores, placeat quos sunt veniam voluptatem? Dolore doloremque ea molestias quam velit! At doloremque magni optio praesentium provident sapiente soluta voluptatum.
+                </div>
+            </div>
+            <div class="post"></div>
+        </div>
+    </main>
+    </body>
 </html>
