0b0495653a88517d6d2e1495690e334c0c3465b1
Petter77 <ppawlinski77@gmail.com>
2023-11-22 22:43:40 +0100

    Added footer, logo, changed main stylesheet file name to indexStyle.css

--- index.php
+++ index.php
@@ -1,13 +1,14 @@
 <!doctype html>
 <html lang="pl">
 <head>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="indexStyle.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap"
           rel="stylesheet">
     <meta charset="UTF-8">
     <meta name="author" content="Patryk Pawliński">
+    <meta name="logo-author" content="Patryk Pawliński">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>JTPPPCWOW</title>
@@ -15,11 +16,13 @@
     <body>
         <nav>
             <div id="nav-left">
-                <div id="logo">logo</div>
+                <div id="logo">
+                    <img src="img/placeholder.svg" alt="website logo">
+                </div>
             </div>
             <div id="nav-right">
                 <a href="" class="nav-elem"><div></div></a>
-                <a href="" id="register-button" class="nav-elem"><div>Register</div></a>
+                <a href="register.php" id="register-button" class="nav-elem"><div>Register</div></a>
                 <a href="" id="login-button" class="nav-elem"><div>Login</div></a>
             </div>
         </nav>
@@ -41,8 +44,17 @@
                     Lorem ipsum dolor sit amet, consectetur adipisicing elit. A, accusantium animi, commodi consequatur ducimus eos ex excepturi exercitationem id laboriosam maiores non nulla optio quam quidem rem repellendus rerum tempore voluptas voluptate? A ad aliquid autem, culpa dicta dolores earum fugit illum in maxime molestiae nesciunt nihil nobis nostrum nulla optio, pariatur quas quia quo repellendus similique soluta sunt suscipit tenetur veritatis. Aspernatur beatae cumque, dolores incidunt maxime nostrum quae quibusdam quos reiciendis, tenetur unde velit. Dolor et ipsa maiores, placeat quos sunt veniam voluptatem? Dolore doloremque ea molestias quam velit! At doloremque magni optio praesentium provident sapiente soluta voluptatum.
                 </div>
             </div>
-            <div class="post"></div>
-        </div>
+            <div class="post">
+                <div class="post-author">
+                    Test
+                </div>
+                <div class="post-content">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. A, accusantium animi, commodi consequatur ducimus eos ex excepturi exercitationem id laboriosam maiores non nulla optio quam quidem rem repellendus rerum tempore voluptas voluptate? A ad aliquid autem, culpa dicta dolores earum fugit illum in maxime molestiae nesciunt nihil nobis nostrum nulla optio, pariatur quas quia quo repellendus similique soluta sunt suscipit tenetur veritatis. Aspernatur beatae cumque, dolores incidunt maxime nostrum quae quibusdam quos reiciendis, tenetur unde velit. Dolor et ipsa maiores, placeat quos sunt veniam voluptatem? Dolore doloremque ea molestias quam velit! At doloremque magni optio praesentium provident sapiente soluta voluptatum.
+                </div>
+            </div>
     </main>
+        <footer>
+            <div>Icons made from <a href="https://www.onlinewebfonts.com/icon">svg icons</a> is licensed by CC BY 4.0</div>
+        </footer>
     </body>
 </html>
